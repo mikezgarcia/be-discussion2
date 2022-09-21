1d52dcbc9e3ddf526363f8843438afb4c9ffafa2
Michael Garcia <michaelgarcia101@gmail.com>
2022-09-21 19:28:45 +0800

    Initial commit

---
+++ src/main/java/mikezgarcia/bediscussion2/BeDiscussion2Application.java
@@ -0,0 +1,13 @@
+package mikezgarcia.bediscussion2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeDiscussion2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeDiscussion2Application.class, args);
+	}
+
+}
---
+++ src/test/java/mikezgarcia/bediscussion2/BeDiscussion2ApplicationTests.java
@@ -0,0 +1,13 @@
+package mikezgarcia.bediscussion2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeDiscussion2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
